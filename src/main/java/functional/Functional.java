e71510da44b9c71fc6b209b0b47f8c87b5544fed
alexfranz <ut1339@gmail.com>
2018-07-13 22:55:38 +0300

    * fizz buzz was refactored to functional paradigm

--- src/main/java/functional/Functional.java
+++ src/main/java/functional/Functional.java
@@ -1,7 +1,6 @@
 package functional;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -10,19 +9,22 @@ import java.util.stream.Stream;
 public class Functional {
     public static Consumer<String> print = System.out::println;
 
-    private static TripleFunction<List<Integer>, Integer, Integer, List<Integer>> primes = (acc, k, n) -> (k > n) ?
-            acc : curry(Functional.primes).apply(acc.stream().anyMatch(i -> k % i == 0) ? acc : Stream.concat(acc.stream(), Stream.of(k)).collect(Collectors.toList())).apply(k + 1).apply(n);
+    private static Map<Integer, String> fizzbuzz = new TreeMap<>();
+
+    static {
+        fizzbuzz.put(3, "Fizz");
+        fizzbuzz.put(5, "Buzz");
+    }
+
+    private static TripleFunction<List<Integer>, Integer, Integer, List<Integer>> primes = (acc, k, n) -> (k > n) ? acc :
+            curry(Functional.primes).apply(acc.stream().anyMatch(i -> k % i == 0) ? acc : Stream.concat(acc.stream(), Stream.of(k)).collect(Collectors.toList())).apply(k + 1).apply(n);
 
     public static IntFunction<List<Integer>> prime = n -> curry(primes).apply(new ArrayList<>()).apply(2).apply(n);
 
     private static UnaryOperator<Long> factorial = x -> x == 0 ? 1 : x * Functional.factorial.apply(x - 1);
 
-    private static IntFunction<String> check = i -> {
-        if (i % 15 == 0) return "FizzBuzz";
-        if (i % 3 == 0) return "Fizz";
-        if (i % 5 == 0) return "Buzz";
-        return String.valueOf(i);
-    };
+    private static IntFunction<String> check = i ->
+            Optional.ofNullable(fizzbuzz.keySet().stream().filter(n -> i % n == 0).map(fizzbuzz::get).collect(Collectors.joining(""))).filter(s -> !s.isEmpty()).orElse(String.valueOf(i));
 
     public static IntConsumer fizzyBuzzy = n -> IntStream.range(1, n).forEach(i -> print.accept(check.apply(i)));
 
