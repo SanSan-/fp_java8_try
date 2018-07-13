6fa2a736d6e0bac6c4c1cbefe7c47820ba1253c3
alexfranz <ut1339@gmail.com>
2018-07-13 23:49:51 +0300

    * Google code style was applied

--- src/main/java/functional/Functional.java
+++ src/main/java/functional/Functional.java
@@ -7,39 +7,48 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Functional {
-    public static Consumer<String> print = System.out::println;
 
-    private static Map<Integer, String> fizzbuzz = new TreeMap<>();
+	public static Consumer<String> print = System.out::println;
 
-    static {
-        fizzbuzz.put(3, "Fizz");
-        fizzbuzz.put(5, "Buzz");
-    }
+	private static Map<Integer, String> fizzbuzz = new TreeMap<>();
 
-    private static TripleFunction<List<Integer>, Integer, Integer, List<Integer>> primes = (acc, k, n) -> (k > n) ? acc :
-            curry(Functional.primes).apply(acc.stream().anyMatch(i -> k % i == 0) ? acc : Stream.concat(acc.stream(), Stream.of(k)).collect(Collectors.toList())).apply(k + 1).apply(n);
+	static {
+		fizzbuzz.put(3, "Fizz");
+		fizzbuzz.put(5, "Buzz");
+	}
 
-    public static IntFunction<List<Integer>> prime = n -> curry(primes).apply(new ArrayList<>()).apply(2).apply(n);
+	private static TripleFunction<List<Integer>, Integer, Integer, List<Integer>> primes = (acc, k, n) ->
+			(k > n) ? acc : curry(Functional.primes).apply(acc.stream().anyMatch(i -> k % i == 0) ? acc
+					: Stream.concat(acc.stream(), Stream.of(k)).collect(Collectors.toList())).apply(k + 1)
+					.apply(n);
 
-    private static UnaryOperator<Long> factorial = x -> x == 0 ? 1 : x * Functional.factorial.apply(x - 1);
+	public static IntFunction<List<Integer>> prime = n -> curry(primes).apply(new ArrayList<>())
+			.apply(2).apply(n);
 
-    private static IntFunction<String> check = i ->
-            Optional.ofNullable(fizzbuzz.keySet().stream().filter(n -> i % n == 0).map(fizzbuzz::get).collect(Collectors.joining(""))).filter(s -> !s.isEmpty()).orElse(String.valueOf(i));
+	private static UnaryOperator<Long> factorial = x -> x == 0 ? 1
+			: x * Functional.factorial.apply(x - 1);
 
-    public static IntConsumer fizzyBuzzy = n -> IntStream.range(1, n).forEach(i -> print.accept(check.apply(i)));
+	private static IntFunction<String> check = i -> Optional.ofNullable(
+			fizzbuzz.keySet().stream().filter(n -> i % n == 0).map(fizzbuzz::get)
+					.collect(Collectors.joining(""))).filter(s -> !s.isEmpty()).orElse(String.valueOf(i));
 
-    public static IntFunction<List<String>> fizzBuzzList = n -> IntStream.range(1, n).mapToObj(i -> check.apply(i)).collect(Collectors.toList());
+	public static IntConsumer fizzyBuzzy = n -> IntStream.range(1, n)
+			.forEach(i -> print.accept(check.apply(i)));
 
-    public static Long fact(Long x) {
-        return factorial.apply(x);
-    }
+	public static IntFunction<List<String>> fizzBuzzList = n -> IntStream.range(1, n)
+			.mapToObj(i -> check.apply(i)).collect(Collectors.toList());
 
+	public static Long fact(Long x) {
+		return factorial.apply(x);
+	}
 
-    public static <P1, P2, P3, R> Function<P1, Function<P2, Function<P3, R>>> curry(TripleFunction<P1, P2, P3, R> f) {
-        return p1 -> p2 -> p3 -> f.apply(p1, p2, p3);
-    }
+	public static <P1, P2, P3, R> Function<P1, Function<P2, Function<P3, R>>> curry(
+			TripleFunction<P1, P2, P3, R> f) {
+		return p1 -> p2 -> p3 -> f.apply(p1, p2, p3);
+	}
 
-    public interface TripleFunction<E, T, U, R> {
-        R apply(E e, T t, U u);
-    }
+	public interface TripleFunction<E, T, U, R> {
+
+		R apply(E e, T t, U u);
+	}
 }
--- src/test/java/FunctionalTest.java
+++ src/test/java/FunctionalTest.java
@@ -8,24 +8,27 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class FunctionalTest {
-    @Test
-    public void primeTest() {
-        List<Integer> primes = Functional.prime.apply(100);
-        assertNotNull(primes);
-        assertEquals(25, primes.size());
-        assertEquals(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97), primes);
-    }
 
-    @Test
-    public void factorialTest() {
-        assertEquals(java.util.Optional.of(6L), java.util.Optional.of(Functional.fact(3L)));
-    }
+	@Test
+	public void primeTest() {
+		List<Integer> primes = Functional.prime.apply(100);
+		assertNotNull(primes);
+		assertEquals(25, primes.size());
+		assertEquals(Arrays
+				.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79,
+						83, 89, 97), primes);
+	}
 
-    @Test
-    public void fizzBuzzTest() {
-        Functional.fizzyBuzzy.accept(100);
-        List<String> fzBz = Functional.fizzBuzzList.apply(100);
-        fzBz.forEach(fb -> Functional.print.accept(fb));
-        assertEquals(99, fzBz.size());
-    }
+	@Test
+	public void factorialTest() {
+		assertEquals(java.util.Optional.of(6L), java.util.Optional.of(Functional.fact(3L)));
+	}
+
+	@Test
+	public void fizzBuzzTest() {
+		Functional.fizzyBuzzy.accept(100);
+		List<String> fzBz = Functional.fizzBuzzList.apply(100);
+		fzBz.forEach(fb -> Functional.print.accept(fb));
+		assertEquals(99, fzBz.size());
+	}
 }
